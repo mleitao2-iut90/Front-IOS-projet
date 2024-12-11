e6772d3d79170ebe63b4dca75c6409b853b9baec
mleitao2-iut90 <leitaomanuel54360@gmail.com>
2024-12-11 16:54:34 +0100

    init

---
+++ Uber-IT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uber-IT
+//
+//  Created by leitao manuel on 11/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, worldu!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uber-IT/Uber_ITApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_ITApp.swift
+//  Uber-IT
+//
+//  Created by leitao manuel on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Uber_ITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
